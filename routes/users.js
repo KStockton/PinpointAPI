48b50863850b273dfbc676b2d24ffde8c37d7d0b
kingstockton@gmail.com <>
2023-02-16 14:04:26 -0700

    Update routes.

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 // register
 
 router.post('/register', async(req, res) => {
+  console.log('register')
   try {
     //generate  new password
     const salt = await bcrypt.genSalt(10)
